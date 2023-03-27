253c50db29d693538a5dfcae4d94d87c4883c21a
Riki <ricardocr987@gmail.com>
2023-03-27 11:05:06 +0200

    detect ErrorBoundaries

--- app/brick/src/pages/_app.tsx
+++ app/brick/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import { ContextProvider } from '@/components/contexts/ContextProvider';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
+import { Component } from 'react';
 
 const NavItems = [
   { label: "Home", url: "", key: 1 },
@@ -23,9 +24,41 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <ContextProvider>
         <Navbar NavItems={NavItems} />
-        <Component {...pageProps} />
+        <ErrorBoundary>
+          <Component {...pageProps} />
+        </ErrorBoundary>
         <Footer />
       </ContextProvider>
     </>
   );
+}
+
+class ErrorBoundary extends Component<{children: React.ReactNode}, { hasError: boolean, error: any, info: any }> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      info: null
+    };
+  }
+  componentDidCatch(error, info) {
+    this.setState({
+      hasError: true,
+      error: error,
+      info: info
+    });
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Oops, something went wrong :(</h1>
+          <p>The error: {this.state.error.toString()}</p>
+          <p>Where it occured: {this.state.info.componentStack}</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
 }
