6c0ec3ada7bf7c36ff1e938dd1e6212e02777667
Riki <ricardocr987@gmail.com>
2023-08-20 21:38:29 +0200

    feat: aleph messages on cnft & fix params parsing

---
+++ transaction-builder/src/aleph.ts
@@ -0,0 +1,29 @@
+import { SOLAccount } from 'aleph-sdk-ts/dist/accounts/solana.js';
+import { Publish as publishPost } from 'aleph-sdk-ts/dist/messages/post/index.js';
+import { ItemType } from 'aleph-sdk-ts/dist/messages/types/base.js';
+
+type MessageContent = {
+    seller: string;
+    signer: string;
+    units: number;
+    paymentMint: string;
+    totalAmount: number;
+}
+
+export async function generateAlephMessage(content: MessageContent, messagesSigner: SOLAccount): Promise<string> {
+    try {
+        const resp = await publishPost({
+            account: messagesSigner,
+            postType: 'Purchase',
+            content,
+            channel: 'BRICK_V1',
+            APIServer: 'https://api2.aleph.im',
+            inlineRequested: true,
+            storageEngine: ItemType.inline
+        });
+
+        return resp.item_hash;
+    } catch(e) {
+      throw new Error(`Error posting aleph message: ${e}`)
+    }
+}
--- transaction-builder/src/index.ts
+++ transaction-builder/src/index.ts
@@ -1,13 +1,17 @@
-import { createInitProductTreeTransaction, createRegisterBuyCnftTransaction, queryPurchases } from 'brick-protocol';
+import { Product, createInitProductTreeTransaction, createRegisterBuyCnftTransaction, queryAccounts, queryPurchases } from 'brick-protocol';
 import express, { Express, Request, Response } from 'express';
+import { ImportAccountFromPrivateKey } from 'aleph-sdk-ts/dist/accounts/solana.js';
 import dotenv from 'dotenv';
 import { Connection, PublicKey } from '@solana/web3.js';
+import { generateAlephMessage } from './aleph.js';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 const rpc = process.env.RPC;
+const messagesKey = process.env.MESSAGES_KEY;
+const indexerApi = process.env.INDEXER_API;
 
 app.use(express.json());
 
@@ -35,9 +39,20 @@ app.get('/registerBuy', async (req: Request, res: Response) => {
     try {
         if (!rpc) throw new Error('Error: Server rpc not configured');
         const connection = new Connection(rpc);
-        const { signer, marketplace,  product, paymentMint, seller, marketplaceAuth, merkleTree, params } = req.query;
-        if (!signer || !marketplace || !paymentMint) throw new Error('Error: Missing account');
-        
+
+        const { signer, marketplace, product, paymentMint, seller, marketplaceAuth, params } = req.query;
+        if (!signer || !marketplace || !paymentMint || !product || !seller || !marketplaceAuth || !params) {
+          throw new Error('Error: Missing required information');
+        }        
+
+        if (!messagesKey) throw new Error('Error: MessagesKey not configured');
+        const messagesSigner = ImportAccountFromPrivateKey(Uint8Array.from(JSON.parse(messagesKey)));
+
+        if (!indexerApi) throw new Error('Error: Indexer server not configured');
+        const productResponse = await queryAccounts(indexerApi, { accounts: [product] });
+        const productInfo = productResponse[0].data as Product;
+        const itemHash = await generateAlephMessage({ seller, signer, units: Number(params.amount), paymentMint, totalAmount: Number(productInfo.sellerConfig.productPrice) * params.amount }, messagesSigner);
+        console.log('merkle', productInfo)
         const accounts = {
             signer: new PublicKey(signer),
             marketplace: new PublicKey(marketplace),
@@ -45,10 +60,16 @@ app.get('/registerBuy', async (req: Request, res: Response) => {
             paymentMint: new PublicKey(paymentMint),
             seller: new PublicKey(seller),
             marketplaceAuth: new PublicKey(marketplaceAuth),
-            merkleTree: new PublicKey(merkleTree),
+            merkleTree: new PublicKey(productInfo.merkleTree),
         };
+        const parsedParams = {
+            rewardsActive: params.rewardsActive === 'true' ? true : false,
+            amount: Number(params.amount),
+            name: params.name,
+            uri: `https://api1.aleph.im/api/v0/messages.json?hashes=${itemHash}`,
+        }
+        const transaction = await createRegisterBuyCnftTransaction(connection, accounts, parsedParams);     
 
-        const transaction = await createRegisterBuyCnftTransaction(connection, accounts, params);        
         res.status(200).send({ transaction: Buffer.from(transaction.serialize()).toString('base64') });
     } catch (error) {
         res.status(500).send({ error: error.message });
